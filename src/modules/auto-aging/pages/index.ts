301fc9f3d97816290b6853bd2410181f863f3a72
jy0ung <112876621+jy0ung@users.noreply.github.com>
2026-04-15 14:32:28 +0800

    introduce auto aging module page barrel

---
+++ src/modules/auto-aging/pages/index.ts
@@ -0,0 +1,8 @@
+export { default as AutoAgingDashboardPage } from '@/pages/auto-aging/AutoAgingDashboard';
+export { default as VehicleExplorerPage } from '@/pages/auto-aging/VehicleExplorer';
+export { default as VehicleDetailPage } from '@/pages/auto-aging/VehicleDetail';
+export { default as ImportCenterPage } from '@/pages/auto-aging/ImportCenter';
+export { default as DataQualityPage } from '@/pages/auto-aging/DataQuality';
+export { default as SLAAdminPage } from '@/pages/auto-aging/SLAAdmin';
+export { default as MappingAdminPage } from '@/pages/auto-aging/MappingAdmin';
+export { default as ImportHistoryPage } from '@/pages/auto-aging/ImportHistory';
--- src/modules/auto-aging/routes.tsx
+++ src/modules/auto-aging/routes.tsx
@@ -1,24 +1,26 @@
 import { Route } from 'react-router-dom';
-import AutoAgingDashboard from '@/pages/auto-aging/AutoAgingDashboard';
-import VehicleExplorer from '@/pages/auto-aging/VehicleExplorer';
-import VehicleDetail from '@/pages/auto-aging/VehicleDetail';
-import ImportCenter from '@/pages/auto-aging/ImportCenter';
-import DataQuality from '@/pages/auto-aging/DataQuality';
-import SLAAdmin from '@/pages/auto-aging/SLAAdmin';
-import MappingAdmin from '@/pages/auto-aging/MappingAdmin';
-import ImportHistory from '@/pages/auto-aging/ImportHistory';
+import {
+  AutoAgingDashboardPage,
+  VehicleExplorerPage,
+  VehicleDetailPage,
+  ImportCenterPage,
+  DataQualityPage,
+  SLAAdminPage,
+  MappingAdminPage,
+  ImportHistoryPage,
+} from '@/modules/auto-aging/pages';
 
 export function AutoAgingRoutes() {
   return (
     <>
-      <Route path="/auto-aging" element={<AutoAgingDashboard />} />
-      <Route path="/auto-aging/vehicles" element={<VehicleExplorer />} />
-      <Route path="/auto-aging/vehicles/:chassisNo" element={<VehicleDetail />} />
-      <Route path="/auto-aging/import" element={<ImportCenter />} />
-      <Route path="/auto-aging/quality" element={<DataQuality />} />
-      <Route path="/auto-aging/sla" element={<SLAAdmin />} />
-      <Route path="/auto-aging/mappings" element={<MappingAdmin />} />
-      <Route path="/auto-aging/history" element={<ImportHistory />} />
+      <Route path="/auto-aging" element={<AutoAgingDashboardPage />} />
+      <Route path="/auto-aging/vehicles" element={<VehicleExplorerPage />} />
+      <Route path="/auto-aging/vehicles/:chassisNo" element={<VehicleDetailPage />} />
+      <Route path="/auto-aging/import" element={<ImportCenterPage />} />
+      <Route path="/auto-aging/quality" element={<DataQualityPage />} />
+      <Route path="/auto-aging/sla" element={<SLAAdminPage />} />
+      <Route path="/auto-aging/mappings" element={<MappingAdminPage />} />
+      <Route path="/auto-aging/history" element={<ImportHistoryPage />} />
     </>
   );
 }
